514bd537e4510a64e65440003111f66d9847ecc6
Anand Aravindan <anand.aravindan@searshc.com>
2015-07-02 12:13:52 -0500

    Adding max heap size

--- multiple-tomcat-instance/setenv.sh
+++ multiple-tomcat-instance/setenv.sh
@@ -33,8 +33,8 @@
 
 # discourage address map swapping by setting Xms and Xmx to the same value
 # http://confluence.atlassian.com/display/DOC/Garbage+Collector+Performance+Issues
-export CATALINA_OPTS="$CATALINA_OPTS -Xms64m"
-export CATALINA_OPTS="$CATALINA_OPTS -Xmx512m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xms256m"
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx768m"
 
 # Increase maximum perm size for web base applications to 4x the default amount
 # http://wiki.apache.org/tomcat/FAQ/Memoryhttp://wiki.apache.org/tomcat/FAQ/Memory
